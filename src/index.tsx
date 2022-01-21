eb71632af2790fd563b556c9ad367189547ba0a0
kuolintoive <karpinenh@gmail.com>
2022-01-21 04:41:01 -0700

    add entry point

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import { store } from './app/store';
+import { store } from './redux/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
+import GameBoard from './components/GameBoard';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <GameBoard />
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')

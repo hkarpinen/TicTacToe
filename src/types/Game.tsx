23c9b72d33e4b4e9266de4ef330d657a09779b49
kuolintoive <karpinenh@gmail.com>
2022-01-21 04:40:11 -0700

    add game types

---
+++ src/types/Game.tsx
@@ -0,0 +1,23 @@
+export interface Gameboard {
+    rows: BoardRows
+}
+
+export interface BoardRow {
+    items: BoardRowItems
+}
+
+export interface BoardItem {
+    owner: BoardItemOwner
+    id: number
+}
+
+export type BoardRows = [BoardRow, BoardRow, BoardRow];
+export type BoardRowItems = [BoardItem, BoardItem, BoardItem];
+export type BoardItemOwner = GamePlayer | '';
+export type GamePlayer = 'o' | 'x';
+export type BoardItemUpdate = [number, GamePlayer];
+export type GameData = [
+    BoardItem, BoardItem, BoardItem,
+    BoardItem, BoardItem, BoardItem,
+    BoardItem, BoardItem, BoardItem
+]

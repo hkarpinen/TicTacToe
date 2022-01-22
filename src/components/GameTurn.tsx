5c3642eff86ee0f95613c608ab87eaedf13489a1
kuolintoive <karpinenh@gmail.com>
2022-01-22 15:53:53 -0700

    add tie render

--- src/components/GameTurn.tsx
+++ src/components/GameTurn.tsx
@@ -2,22 +2,31 @@ import React from "react";
 import { Container } from "react-bootstrap";
 import { connect, ConnectedProps } from 'react-redux';
 import { RootState } from "../redux/store";
-import { BoardItemOwner, GamePlayer } from "../types/Game";
+import { BoardItemOwner, GameData, GamePlayer } from "../types/Game";
 
 const mapState = (state: RootState) => ({
     nextPlayer: state.gameBoardSlice.player.next,
-    winner: state.gameBoardSlice.game.winner
+    winner: state.gameBoardSlice.game.winner,
+    data: state.gameBoardSlice.game.data
 })
 
 const connector = connect(mapState);
 type ReduxProps = ConnectedProps<typeof connector>
 interface Props extends ReduxProps {
     nextPlayer: GamePlayer,
-    winner: BoardItemOwner
+    winner: BoardItemOwner,
+    data: GameData
 }
 
 
 const GameTurn = (props: Props) => {
+    // Tie
+    if(props.data.filter(item => item.owner === '').length === 0) {
+        return <Container id='game-turn'>
+            <p id='game-turn-title'>Tie!</p>
+        </Container>
+    }
+
     // If the winner is X, display X.
     if(props.winner === 'x') {
         return <Container id='game-turn'>

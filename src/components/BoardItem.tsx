d29167bfea36b29f658eb1f38a4541a219f90fc3
kuolintoive <karpinenh@gmail.com>
2022-01-22 00:32:47 -0700

    add conditional renders

--- src/components/BoardItem.tsx
+++ src/components/BoardItem.tsx
@@ -2,23 +2,43 @@ import React from "react";
 import { Container } from "react-bootstrap";
 import { BoardItemOwner } from "../types/Game";
 import { useAppDispatch, useAppSelector } from '../redux/hooks';
-import { setItemOwner, setCurrentPlayer, setWinner } from '../redux/reducers/Game';
+import { setItemOwner, setWinner } from '../redux/reducers/Game';
 import { RootState } from "../redux/store";
 
 interface Props {
     owner: BoardItemOwner,
-    id: number
+    id: number,
+    winningItem: boolean
 }
 
 export const GameBoardItem = (props: Props) => {
-    const currentPlayer = useAppSelector((state: RootState) => state.gameBoardSlice.player);
+    const currentPlayer = useAppSelector((state: RootState) => state.gameBoardSlice.player.next);
+    const hasWinner = useAppSelector((state: RootState) => state.gameBoardSlice.game.winner !== '');
+    const activeGame = useAppSelector((state: RootState) => state.gameBoardSlice.game.active);
     const dispatch = useAppDispatch();
+
+    if(!activeGame) {
+        return <Container className="game-board-item">
+            <p>{props.owner.toUpperCase()}</p>
+        </Container>
+    }
+
+    if(props.winningItem) {
+        return <Container className="game-board-item">
+            <p style={{ color: '#90EE90' }}>{props.owner.toUpperCase()}</p>
+        </Container>
+    }
+
+    if(hasWinner) {
+        return <Container className="game-board-item">
+            <p>{props.owner.toUpperCase()}</p>
+        </Container>
+    }
+
     return <Container className="game-board-item" onClick={() => {
         // only update the owner if it's not taken.
         if(props.owner === '') {
             dispatch(setItemOwner([props.id, currentPlayer]));
-            const nextPlayer = currentPlayer === 'o' ? 'x' : 'o';
-            dispatch(setCurrentPlayer(nextPlayer));
             dispatch(setWinner());
         }
     }}>

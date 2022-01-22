b130d776d3de9c1c871d7e4fec15a342abab1bcf
kuolintoive <karpinenh@gmail.com>
2022-01-22 14:22:48 -0700

    add documentation

--- src/lib/GameInspector.ts
+++ src/lib/GameInspector.ts
@@ -1,11 +1,12 @@
 import { BoardItemOwner, GameData } from "../types/Game";
 import { BoardItem } from "../types/Game";
 
+// Object typing for win detection.
 export type WinPattern = [number, number, number];
 export type PatternResult = WinPattern | undefined;
-
 type WinnerResult = [BoardItemOwner, PatternResult];
 
+// All winning patterns.
 const WinPatterns: WinPattern[] = [
     [1, 2, 3],
     [4, 5, 6],
@@ -17,6 +18,12 @@ const WinPatterns: WinPattern[] = [
     [3, 5, 7]
 ]
 
+/* Check if a player has won the game.
+1. Iterate each winning pattern.
+2. If all the items in a winning pattern have the same player as their owner, 
+the game has been won.
+3. Return the winner, if there is no winner, return undefined.  
+*/
 export const getWinner = (data: GameData): WinnerResult => {
     let winner: WinnerResult = ['', undefined];
     WinPatterns.forEach(pattern => {
@@ -33,10 +40,13 @@ export const getWinner = (data: GameData): WinnerResult => {
     return winner;
 }
 
+// Check if BoardRowItems share an owner. 
 export const itemsShareOwner = (owner: BoardItemOwner, items: BoardItem[]) => {
     return items.filter(item => item.owner !== owner).length === 0;
 }
 
+
+// Get BoardRowItems using a number range. 
 const getItems = (items: number[], data: GameData) => {
     const targetItems: BoardItem[] = [];
     data.forEach(item => {
